1d15a0c6b560e9256d7cf4913b75ee7f2e1b9e12
André <andregds85@gmail.com>
2024-01-08 05:15:06 -0300

    deploy Linguagem C

---
+++ Introducao/exer1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+main(){
+int num;
+
+printf("Numero : ");
+scanf("%d", &num);
+
+printf("Valor : %d",num);
+system("pause");
+}
---
+++ Introducao/exer2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+main(){
+float valor;
+
+printf(" Digite Valor :");
+scanf("%f",&valor);
+
+printf("Valor Real : %f ",valor);
+
+}

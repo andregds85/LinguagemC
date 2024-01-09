ace4fd61930b3a4761bee2724ed36bbef9233ad9
André <andregds85@gmail.com>
2024-01-09 15:59:59 -0300

    deploy no exercicio 4

---
+++ Introducao/exer4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void){
+float num,quadrado; 
+
+printf("Numero : ");
+scanf("%f",&num);
+quadrado=num*num;
+printf("Resultado : %f",num);
+printf("\n");
+printf("Quadrado : %f",quadrado);
+return 0;
+system("pause");
+}

fad2cfa04a718e1f63a4fd6ebfd7ecda987507c6
André <andregds85@gmail.com>
2024-01-10 02:46:49 -0300

    Alterando campo text para check box

---
+++ Introducao/exer5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void){
+float valor,parte;
+
+printf("Digite um Valor :");
+scanf("%f",&valor);
+parte=valor/5;
+
+
+printf("Quinta Parte : %f ",parte);
+
+return 0;
+system("pause");
+}
